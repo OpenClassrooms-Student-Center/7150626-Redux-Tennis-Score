20f2616693a604e7cca74f17685871868b788882
Etienne Dldc <e.deladonchamps@gmail.com>
2021-05-11 22:29:08 +0200

    Add points before win

--- src/PlayerScore.js
+++ src/PlayerScore.js
@@ -1,13 +1,25 @@
 import { useSelector } from "react-redux";
-import { selectPlayerHasAdvantage, selectPlayerScore } from "./selectors";
+import {
+  selectPlayerHasAdvantage,
+  selectPlayerScore,
+  selectPointBeforeWin,
+} from "./selectors";
 
 export function PlayerScore({ playerId, playerName }) {
   const score = useSelector(selectPlayerScore(playerId));
   const hasAdvantage = useSelector(selectPlayerHasAdvantage(playerId));
+  const pointsBeforeWin = useSelector(selectPointBeforeWin(playerId));
 
   return (
     <div className="player-score">
-      <p>{playerName}</p>
+      <p>
+        {playerName}
+        {pointsBeforeWin === null
+          ? ""
+          : ` (encore ${pointsBeforeWin} ${
+              pointsBeforeWin > 1 ? "points" : "point"
+            })`}
+      </p>
       <p>{(hasAdvantage ? "Avantage " : "") + score}</p>
     </div>
   );
--- src/selectors.js
+++ src/selectors.js
@@ -1,3 +1,32 @@
+export const selectPointBeforeWin = (playerId) => {
+  const otherPlayerId = playerId === "player1" ? "player2" : "player1";
+  return (state) => {
+    if (state.winner) {
+      return null;
+    }
+    if (state.advantage === playerId) {
+      return 1;
+    }
+    if (state.advantage === otherPlayerId) {
+      return 3;
+    }
+    const playerScore = state[playerId];
+    const otherPlayerScore = state[otherPlayerId];
+    const pointsTo40 =
+      playerScore === 0
+        ? 3
+        : playerScore === 15
+        ? 2
+        : playerScore === 30
+        ? 1
+        : 0;
+    if (otherPlayerScore === 40) {
+      return pointsTo40 + 2;
+    }
+    return pointsTo40 + 1;
+  };
+};
+
 export const selectPlayerHasAdvantage = (playerId) => {
   return (state) => state.advantage === playerId;
 };
